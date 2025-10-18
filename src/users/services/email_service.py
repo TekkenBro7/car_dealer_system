2bedb98f40799bcf1c5e402d61ae5c64b816bdb8
Maxim <maks.kreet@mail.ru>
2025-10-18 18:36:19 +0300

    Fix comment

--- src/users/services/email_service.py
+++ src/users/services/email_service.py
@@ -1,9 +1,13 @@
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+from rest_framework import status
+from rest_framework.response import Response
 
 from core.logging import logger
 from users.models import User
+from users.utils import email_verification
 
 
 def send_confirmation_email(user: User, token: str) -> None:
@@ -39,3 +43,30 @@ def send_confirmation_email(user: User, token: str) -> None:
         logger.info("Email sent successfully to %s email %s", user, to)
     except Exception as e:
         logger.error("Failed to send email: %s", e, exc_info=True)
+
+
+def confirm_user_email(token: str) -> Response:
+    """
+    Validate the token, confirm user's email, and return appropriate Response.
+    """
+    user_pk = email_verification.verify_email_token(token)
+    if not user_pk:
+        return Response(
+            {"detail": "The link is invalid or outdated."},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    try:
+        user = User.objects.get(pk=user_pk)
+    except ObjectDoesNotExist:
+        return Response(
+            {"detail": "The user was not found."},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
+    user.email_confirmed = True
+    user.save()
+    return Response(
+        {"detail": "Email successfully confirmed."},
+        status=status.HTTP_200_OK,
+    )
--- src/users/views.py
+++ src/users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import mixins, status, viewsets
+from rest_framework import mixins, viewsets
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer
@@ -10,7 +10,7 @@ from users.serializers import (
     UserProfileSerializer,
     UserSerializer,
 )
-from users.utils import email_verification
+from users.services.email_service import confirm_user_email
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -36,21 +36,4 @@ class UserProfileViewSet(
 
 class ConfirmEmailView(APIView):
     def get(self, request: Request, token: str) -> Response:
-        user_pk = email_verification.verify_email_token(token)
-        if not user_pk:
-            return Response(
-                {"detail": "The link is invalid or outdated."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        try:
-            user = User.objects.get(pk=user_pk)
-        except User.DoesNotExist:
-            return Response(
-                {"detail": "The user was not found."}, status=status.HTTP_404_NOT_FOUND
-            )
-
-        user.email_confirmed = True
-        user.save()
-        return Response(
-            {"detail": "Email successfully confirmed."}, status=status.HTTP_200_OK
-        )
+        return confirm_user_email(token)
