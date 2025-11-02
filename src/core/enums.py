7017dfc3c2847189e93fd7a421014a8ec156e5ed
Maxim <maks.kreet@mail.ru>
2025-11-02 21:31:47 +0300

    Fix comments

--- src/core/enums.py
+++ src/core/enums.py
@@ -7,3 +7,13 @@ class ViewAction(StrEnum):
     CREATE = "create"
     UPDATE = "update"
     PARTIAL_UPDATE = "partial_update"
+
+
+class HttpMethod(StrEnum):
+    GET = "GET"
+    POST = "POST"
+    PUT = "PUT"
+    PATCH = "PATCH"
+    DELETE = "DELETE"
+    OPTIONS = "OPTIONS"
+    HEAD = "HEAD"
